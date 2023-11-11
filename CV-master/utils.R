efab4419f7ed499f386cbca6490f00ca88a2480d
Anderson A.C. Alves <60412185+alvesand@users.noreply.github.com>
2023-11-11 14:00:34 -0600

    updated (11/11/2023)

--- CV-master/utils.R
+++ CV-master/utils.R
@@ -162,3 +162,42 @@ print_post<- function(cv, glue_template = "default"){
     invisible(cv)
 }
 
+
+generate_latex_table <- function(data, centered, pad) {
+  # Calculate maximum width for each column
+  col_widths <- apply(data, 2, function(col) 
+    paste(round(max(nchar(as.character(col))*0.22)+pad), 
+          "cm", sep = ""))    # Add some padding
+  
+  if (centered == T){cat("\\begin{tabular}{|", paste(rep(">{\\centering\\arraybackslash}p{"  , ncol(data)), col_widths, "} |", collapse = ""), "}\n")}
+  else{cat("\\begin{tabular}{|", paste(rep("p{"  , ncol(data)), col_widths, "} |", collapse = ""), "}\n")}
+  cat("\\hline\n")
+  cat("\\textbf{", paste(names(data), collapse = "} & \\textbf{"), "} \\\\\n")  # Bold column names
+  cat("\\hline\n")
+  for (row in 1:nrow(data)) {
+    cat(paste(data[row, ], collapse = " & "), " \\\\\n")
+  }
+  cat("\\hline\n")
+  cat("\\end{tabular}\n")
+}
+
+count_authors = function(bibname)
+{   
+  library(RefManageR)
+  mybib <- RefManageR::ReadBib(bibname, check = FALSE)
+  author_list = c(0,0,0)
+  for (i in 1:length(mybib)) {
+    paper = mybib[i]
+    for (j in 1:length(paper$author)) {
+      if (length (paper$author[j]$family) > 1) {next}
+      else if (paper$author[j]$family == "Alves")
+      {
+        if (j==1){author_list[1]=author_list[1]+1}
+        else if (j>1&j<length(paper$author)){author_list[2]=author_list[2]+1}
+        else {author_list[3]=author_list[3]+1}
+      }
+    }
+  }
+  return(author_list)
+}
+
